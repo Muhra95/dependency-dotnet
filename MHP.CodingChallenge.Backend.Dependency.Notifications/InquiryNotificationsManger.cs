bf43418be60329a63dff3966ce5af00373ed1941
Muhra95 <45274412+Muhra95@users.noreply.github.com>
2021-09-30 16:40:55 +0200

    coding-challenges

---
+++ MHP.CodingChallenge.Backend.Dependency.Notifications/InquiryNotificationsManger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MHP.CodingChallenge.Backend.Dependency.Inquiry;
+
+namespace MHP.CodingChallenge.Backend.Dependency.Notifications
+{
+    public class InquiryNotificationsManger : InquiryService
+    {
+        private readonly EmailHandler emailHandler;
+        private readonly PushNotificationHandler pushNotificationHandler;
+        private readonly InquiryService inquiryService;
+
+        public InquiryNotificationsManger(EmailHandler emailHandler, PushNotificationHandler pushNotificationHandler, InquiryService inquiryService)
+        {
+            this.emailHandler = emailHandler;
+            this.pushNotificationHandler = pushNotificationHandler;
+            this.inquiryService = inquiryService;
+        }
+
+        public override void Create(Inquiry.Inquiry inquiry)
+        {
+            base.Create(inquiry);
+            Submit(inquiry);
+        }
+
+        public void Submit(Inquiry.Inquiry inquiry)
+        {
+            this.emailHandler.SendEmail(inquiry);
+            this.pushNotificationHandler.SendNotification(inquiry);
+
+        }
+
+
+    }
+}
